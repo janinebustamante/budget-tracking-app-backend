d83710576ff52c24164b6f1e4bc7e46a386bcd44
Janine Roxanne Bustamante <janineroxanne.bustamante@gmail.com>
2021-04-25 18:54:47 +0800

    prevent duplicate category name

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,71 +1,72 @@
-const Category = require('../models/Category');
-
+const Category = require("../models/Category");
 
 //add category name
 module.exports.addCategory = async (userId, categoryName, categoryType) => {
+  if (!userId) {
+    throw new Error("User ID is missing.");
+  }
 
-    if (!userId) {
-        throw new Error('User ID is missing.')
-    }
+  if (!categoryName) {
+    throw new Error("Category name is missing.");
+  }
 
-    if (!categoryName) {
-        throw new Error('Category name is missing.')
-    }
+  if (!categoryType) {
+    throw new Error("Category type is missing.");
+  }
 
-    if (!categoryType) {
-        throw new Error('Category type is missing.')
-    }
+  const userCategories = await module.exports.getCategories(userId);
+  const sameCategoryName = userCategories.find(
+    (category) => category.categoryName === categoryName
+  );
 
-    const category = new Category({
-        userId,
-        categoryName,
-        categoryType
-    })
+  if (sameCategoryName) {
+    throw new Error(`${categoryName} already exists`);
+  }
 
-    const createdCategory = await category.save();
-    return createdCategory;
-}
+  const category = new Category({
+    userId,
+    categoryName,
+    categoryType,
+  });
 
+  const createdCategory = await category.save();
+  return createdCategory;
+};
 
 //get/list all categories of user
 module.exports.getCategories = async (userId) => {
+  if (!userId) {
+    throw new Error("User ID is missing.");
+  }
 
-    if (!userId) {
-        throw new Error('User ID is missing.')
-    }
-
-    const categories = await Category.find({ userId });
-    return categories;
-}
-
+  const categories = await Category.find({ userId });
+  return categories;
+};
 
 //get specific category
 
-
 //update a category
-module.exports.updateCategory = async (categoryId, categoryName) => {   
+module.exports.updateCategory = async (categoryId, categoryName) => {
+  const updates = {};
+  if (categoryName) {
+    updates.categoryName = categoryName;
+  }
 
-    const updates = {}
-    if (categoryName) {
-        updates.categoryName = categoryName
-    }
-
-    const category = Category.findByIdAndUpdate(categoryId, updates);
-    return category;
-}
-
+  const category = Category.findByIdAndUpdate(categoryId, updates);
+  return category;
+};
 
 //delete a category
 module.exports.deleteCategory = async (userId, categoryId) => {
-    const category = await Category.findOne({ _id: categoryId })
+  const category = await Category.findOne({ _id: categoryId });
 
-    if (!category) {
-        throw new Error('Category not found.')
-    }
+  if (!category) {
+    throw new Error("Category not found.");
+  }
 
-    if (category.userId !== userId) {
-        throw new Error('Category not found.')
-    }
+  if (category.userId !== userId) {
+    throw new Error("Category not found.");
+  }
 
-    await category.delete();
-}
+  await category.delete();
+};
