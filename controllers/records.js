96b5640d87c8019ddeeb9e2b829f9f5e64391456
Janine Roxanne Bustamante <janineroxanne.bustamante@gmail.com>
2021-04-28 21:39:11 +0800

    edited record model, route, controller

--- controllers/records.js
+++ controllers/records.js
@@ -1,53 +1,39 @@
-const Record = require('../models/Record');
-
+const Record = require("../models/Record");
 
 //add a record
-module.exports.addRecord = async (categoryId, amount, description, createdOn, userId) => {
-    
-    if (!categoryId) {
-        throw new Error('Category ID is missing.')
-    }
+module.exports.addRecord = async (categoryId, amount, description, userId) => {
+  if (!categoryId) {
+    throw new Error("Category ID is missing.");
+  }
 
-    if (!amount) {
-        throw new Error('Amount missing.')
-    }
+  if (!amount) {
+    throw new Error("Amount missing.");
+  }
 
-    if (!description) {
-        throw new Error('Description is missing.')
-    }
+  if (!description) {
+    throw new Error("Description is missing.");
+  }
 
-    // if (!createdOn) {
-    //     throw new Error('Date is missing.')
-    // }
+  const record = new Record({
+    categoryId,
+    amount,
+    description,
+    userId,
+  });
 
-    // if (!userId) {
-    //     throw new Error('User ID is missing.')
-    // }
-    
-    const record = new Record({
-        categoryId,
-        amount,
-        description,
-        createdOn,
-        userId
-    })
-
-    const createdRecord = await record.save();
-    return createdRecord;
-}
-
+  const createdRecord = await record.save();
+  return createdRecord;
+};
 
 //get/list all records
 module.exports.getRecords = async (userId) => {
+  if (!userId) {
+    throw new Error("User ID is missing.");
+  }
 
-    if (!userId) {
-        throw new Error('User ID is missing.')
-    }
-
-    const records = await Record.find({userId});
-    return records;
-}
-
+  const records = await Record.find({ userId });
+  return records;
+};
 
 //retrieve a specific record
 // module.exports.getRecord = async (userId) => {
@@ -60,43 +46,47 @@ module.exports.getRecords = async (userId) => {
 //     return record;
 // }
 
-
 //update a record
-module.exports.updateRecord = async (recordId, categoryId, amount, description, createdOn) => {   
-    const updates = {}
+module.exports.updateRecord = async (
+  recordId,
+  categoryId,
+  amount,
+  description,
+  createdOn
+) => {
+  const updates = {};
 
-    if (categoryId) {
-        updates.categoryId = categoryId
-    }
+  if (categoryId) {
+    updates.categoryId = categoryId;
+  }
 
-    if (amount) {
-        updates.amount = amount
-    }
+  if (amount) {
+    updates.amount = amount;
+  }
 
-    if (description) {
-        updates.description = description
-    }
+  if (description) {
+    updates.description = description;
+  }
 
-    if (createdOn) {
-        updates.createdOn = createdOn
-    }
+  if (createdOn) {
+    updates.createdOn = createdOn;
+  }
 
-    const record = Record.findByIdAndUpdate(recordId, updates);
-    return record;
-}
-
+  const record = Record.findByIdAndUpdate(recordId, updates);
+  return record;
+};
 
 //delete a record
 module.exports.deleteRecord = async (userId, recordId) => {
-    const record = await Record.findOne({ _id: recordId })
+  const record = await Record.findOne({ _id: recordId });
 
-    if (!record) {
-        throw new Error('Record not found.')
-    }
+  if (!record) {
+    throw new Error("Record not found.");
+  }
 
-    if (record.userId !== userId) {
-        throw new Error('Record not found.')
-    }
+  if (record.userId !== userId) {
+    throw new Error("Record not found.");
+  }
 
-    await record.delete();
-}
+  await record.delete();
+};
--- routes/records.js
+++ routes/records.js
@@ -1,40 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../auth');
-const RecordController = require('../controllers/records');
-
+const auth = require("../auth");
+const RecordController = require("../controllers/records");
 
 //add a record
-router.post('/', auth.verify, async (req, res) => {
-    const { categoryId, amount, description, createdOn } = req.body;
-    const token = req.headers.authorization;
-    const payload = auth.decode(token);
-    const userId = payload.id;
-    //createdOn??????
+router.post("/", auth.verify, async (req, res) => {
+  const { categoryId, amount, description } = req.body;
+  const token = req.headers.authorization;
+  const payload = auth.decode(token);
+  const userId = payload.id;
+  //createdOn??????
 
-    try {
-        const record = await RecordController.addRecord(categoryId, amount, description, createdOn, userId)
-        res.json(record)
-    } catch (err) {
-        res.status(500).json({err: err.message})
-    }
-})
-
+  try {
+    const record = await RecordController.addRecord(
+      categoryId,
+      amount,
+      description,
+      userId
+    );
+    res.json(record);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 //retrieve all records
-router.get('/', auth.verify, async (req, res) => {
-    const token = req.headers.authorization;
-    const payload = auth.decode(token);
-    const userId = payload.id;
+router.get("/", auth.verify, async (req, res) => {
+  const token = req.headers.authorization;
+  const payload = auth.decode(token);
+  const userId = payload.id;
 
-    try {
-        const records = await RecordController.getRecords(userId)
-        res.json(records);
-    } catch (err) {
-        res.status(500).json({err: err.message});
-    }
-})
-
+  try {
+    const records = await RecordController.getRecords(userId);
+    res.json(records);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 //retrieve a specific record
 // router.get('/:recordId', auth.verify, async (req, res) => {
@@ -48,38 +50,40 @@ router.get('/', auth.verify, async (req, res) => {
 //     }
 // })
 
-
 //update/edit a record
 //how to update categories???????
-router.put('/:recordId', auth.verify, async (req, res) => {
-    const recordId = req.params.recordId
-    const { categoryId, amount, description, createdOn } = req.body
+router.put("/:recordId", auth.verify, async (req, res) => {
+  const recordId = req.params.recordId;
+  const { categoryId, amount, description, createdOn } = req.body;
 
-    try {
-        const record = await RecordController.updateRecord(recordId, categoryId, amount, description, createdOn)
-        res.json({success: true});
-    } catch (err) {
-        res.status(500).json({err: err.message})
-    }
-})
-
+  try {
+    const record = await RecordController.updateRecord(
+      recordId,
+      categoryId,
+      amount,
+      description,
+      createdOn
+    );
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 //delete a record
-router.delete('/:recordId', auth.verify, async (req, res) => {
-    const recordId = req.params.recordId;
+router.delete("/:recordId", auth.verify, async (req, res) => {
+  const recordId = req.params.recordId;
 
-    const token = req.headers.authorization;
-    const payload = auth.decode(token);
-    const userId = payload.id;
+  const token = req.headers.authorization;
+  const payload = auth.decode(token);
+  const userId = payload.id;
 
-    try {
-        await RecordController.deleteRecord(userId, recordId)
-        res.json({success: true});
-    } catch (err) {
-        res.status(500).json({err: err.message})
-    }
-})
-
-
+  try {
+    await RecordController.deleteRecord(userId, recordId);
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 module.exports = router;
