ae0d012a824854121e0a1d300eb78e916823e5e8
Janine Roxanne Bustamante <janineroxanne.bustamante@gmail.com>
2021-05-12 16:46:21 +0800

    allowed CORS access to frontend application

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const express = require('express');
+const express = require("express");
 const api = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 
+// api.use(cors());
+// api.options("*", cors())
 
-api.use(cors());
+const corsOptions = {
+  origin: "https://budget-tracking-app-rho.vercel.app",
+  optionsSuccessStatus: 200,
+};
+api.use(cors(corsOptions));
 
 const connectionString = process.env.MONGO_CONNECTION;
 mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
-mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas'));
-
+mongoose.connection.once("open", () =>
+  console.log("Now connected to MongoDB Atlas")
+);
 
 api.use(express.json());
-api.use(express.urlencoded({extended: true}));
+api.use(express.urlencoded({ extended: true }));
 
+const usersRoutes = require("./routes/users");
+const categoriesRoutes = require("./routes/categories");
+const recordsRoutes = require("./routes/records");
 
-const usersRoutes = require('./routes/users');
-const categoriesRoutes = require('./routes/categories')
-const recordsRoutes = require('./routes/records');
-
-
-api.use('/users', usersRoutes);
-api.use('/categories', categoriesRoutes);
-api.use('/records', recordsRoutes);
-
+api.use("/users", usersRoutes);
+api.use("/categories", categoriesRoutes);
+api.use("/records", recordsRoutes);
 
 // const port = 4000;
-api.listen(process.env.PORT || 4000, () => console.log(`Listening on port ${process.env.PORT}`));
+api.listen(process.env.PORT || 4000, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
